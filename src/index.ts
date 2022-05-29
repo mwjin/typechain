4bf57b9ed9df362732317540e332fc61fd258c42
Minwoo Jeong <jeongmwj@gmail.com>
2022-05-29 19:25:38 +0900

    Define `Block` class

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,24 @@
-import { add } from "./myPackage";
-const hello = () => "Hi";
+import crypto from "crypto";
 
-class Block {
-  constructor(data: string) {}
-  static hello() {
-    console.log("Hello World!");
-    console.log(add(1, 2));
-  }
+interface BlockShape {
+  hash: string;
+  prevHash: string;
+  height: number;
+  data: string;
 }
 
-Block.hello();
+class Block implements BlockShape {
+  public hash: string;
+  constructor(
+    public prevHash: string,
+    public height: number,
+    public data: string
+  ) {
+    this.hash = Block.calculateHash(prevHash, height, data);
+  }
+
+  static calculateHash(prevHash: string, height: number, data: string): string {
+    const toHash = `${prevHash}${height}${data}`;
+    return crypto.createHash("sha256").update(toHash).digest("hex");
+  }
+}
